df2e5a00cd42b29310c1a383c3d8f48c8f8369f5
Nbr Noof <nbrnooof9087@gmail.com>
2024-11-02 05:47:22 +0530

    emotions recognized by selecting options given

--- client/app/page.js
+++ client/app/page.js
@@ -1,9 +1,23 @@
-import Image from "next/image";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import Login from './components/Login';
 
-export default function Home() {
+
+
+
+
+function App() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-    
-    </div>
-  );
+  <Router>
+    <div className="App">
+    <Routes>
+      <Route path="/" element={<Login/>} />
+      <Route path="/home" element={<Home/>} />
+    </Routes>
+  </div>
+</Router>
+);
 }
+
+export default App;
